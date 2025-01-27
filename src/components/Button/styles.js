2137934aef745ad01d7642eb3e1b4357dc5e4997
Sarah6432 <sarah.s.lima973@gmail.com>
2025-01-27 19:08:50 -0300

    calculadora em react

---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #111111;
+    color:#FFC0CB;
+    border-radius: 5px;
+    border: 1.5px solid rgb(59, 59, 59);
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        background-color:rgb(0, 0, 0);
+
+    }
+
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+   width: 100%;
+   height: 75px;
+   background-color: #FFC0CB;
+   border-top-left-radius: 5px;
+   border-top-right-radius: 5px;
+
+   display: flex;
+   align-items: center;
+   justify-content: flex-end;
+
+   font-size: 24px;
+   font-family: Roboto;
+
+    input {
+       width: 100%;
+       height: 75px;
+       background-color:#FFC0CB;
+       border: 0;
+       border-radius: 5px;
+       text-align: right;
+       padding: 0 10px;
+       font-size: 24px;
+       font-family: Roboto;
+       color: #111111;
+    }
+`
